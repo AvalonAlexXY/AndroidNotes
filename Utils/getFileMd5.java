b98e499b4637670afb79e1eaf74f253788ec4497
we <mrzhew@163.com>
2016-03-01 09:24:49 +0800

    Android 获取文件的MD5值

---
+++ Utils/getFileMd5.java
@@ -0,0 +1,35 @@
+/**
+     * 获取文件的md5值
+     *
+     * @param path 文件的全路径名称
+     * @return
+     */
+    private String getFileMd5(String path) {
+        try {
+            // 获取一个文件的特征信息，签名信息。
+            File file = new File(path);
+            // md5
+            MessageDigest digest = MessageDigest.getInstance("md5");
+            FileInputStream fis = new FileInputStream(file);
+            byte[] buffer = new byte[1024];
+            int len = -1;
+            while ((len = fis.read(buffer)) != -1) {
+                digest.update(buffer, 0, len);
+            }
+            byte[] result = digest.digest();
+            StringBuffer sb = new StringBuffer();
+            for (byte b : result) {
+                // 与运算
+                int number = b & 0xff;
+                String str = Integer.toHexString(number);
+                if (str.length() == 1) {
+                    sb.append("0");
+                }
+                sb.append(str);
+            }
+            return sb.toString();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "";
+        }
+    }
